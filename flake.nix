956b3d529524705922b9026ce22da01174ffa79c
Dietrich Daroch <Dietrich@Daroch.me>
2026-05-10 23:07:08 -0400

    Prepare Rust testing support
    This might be annoying right now as dependencies change often, changing the
    environment

--- flake.nix
+++ flake.nix
@@ -7,6 +7,11 @@
     };
     flake-utils.url = "github:numtide/flake-utils";
 
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -19,7 +24,7 @@
       rust-overlay,
       flake-utils,
       ...
-    }:
+    }@inputs:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
@@ -28,11 +33,39 @@
           inherit system overlays;
         };
 
+        # rustManifest = (pkgs.lib.importTOML ./Cargo.toml).package;
         rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        # rustPlatform = pkgs.makeRustPlatform {
+        #   cargo = rustToolchain;
+        #   rustc = rustToolchain;
+        # };
       in
       {
         devShells = {
           default = import ./shell.nix { inherit pkgs rustToolchain; };
+        };
+
+        checks = {
+          # TODO: Enable testing support once dependencies stabilise
+          # tests = rustPlatform.buildRustPackage {
+          #   pname = rustManifest.name;
+          #   version = rustManifest.version;
+          #   src = ./.;
+          #   cargoLock.lockFile = ./Cargo.lock;
+
+          #   nativeBuildInputs = [ pkgs.cargo-nextest ];
+
+          #   buildPhase = "true"; # skip default build
+          #   checkPhase = "cargo nextest run";
+          #   installPhase = "touch $out";
+          # };
+          # TODO: Enable pre-commit checks
+          # pre-commit-check = inputs.git-hooks.lib.${system}.run {
+          #   src = ./.;
+          #   hooks = {
+          #     nixfmt.enable = true;
+          #   };
+          # };
         };
       }
     );
